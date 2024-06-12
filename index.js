7a79d7a99fbd1a70acae6235f36df15ec34d9ceb
vivek628 <143161447+vivek628@users.noreply.github.com>
2024-06-12 21:36:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express=require('express')
 const bodyparser=require('body-parser')
+const adminRoute=require('./Routes/admin')
+const shopRoute=require('./Routes/shop')
 const app=express()
 app.use(bodyparser.urlencoded({extended:false}))
 
-app.use('/add-product',(req,res,next)=>{
-    
-    res.send('<form action="/product" method="POST"><input name="title" placeholder="enter the title"></input><input name="quantity" placeholder="enter the quantity"></input><button type="submit">add</button></form>')
-})
-app.post('/product',(req,res,next)=>{
-    console.log(req.body.title)
-    console.log(req.body.quantity)
-    res.redirect('/')
-})
+app.use('/admin',adminRoute)
+app.use(shopRoute)
+
 
 app.use('/',(req,res,next)=>{
     res.send("<h1>Welcome to express</h1>")
