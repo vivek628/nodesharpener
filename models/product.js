9b9be3a0463c9f200e931cbc085e0ce1cb61d3e9
vivek628 <siloriv2@gmail.com>
2024-07-04 10:39:17 +0530

    my commit

--- models/product.js
+++ models/product.js
@@ -1,29 +1,25 @@
- const db=require('../util/database')
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Sequilize= require('sequelize')
+const sequilize=require('../util/database')
+const Product=sequilize.define('product',{
+  id:{
+    type:Sequilize.INTEGER,
+    autoIncrement:true,
+    allowNull:false,
+    primaryKey:true
+  },
+  title:Sequilize.STRING,
+  price:{
+    type:Sequilize.DOUBLE,
+    allowNull:false
+  },
+  imageUrl:{
+    type:Sequilize.STRING,
+    allowNull:false
+  },
+  description:{
+    type:Sequilize.STRING,
+    allowNull:false
   }
 
-  save() {
-     return db.execute('INSERT INTO products (title,imageUrl,description,price) VALUES(?,?,?,?)',[this.title,this.imageUrl,this.description,this.price])
-      
-  }
-   static deleteproductbyID(id)
-  {
-    return db.execute('DELETE FROM products WHERE products.id=?',[id])
-   
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products')
-   
-  }
-  static FindById(id){
-     return db.execute('SELECT * FROM products WHERE products.id=?',[id])
-   
-  }
-};
+})
+module.exports=Product
